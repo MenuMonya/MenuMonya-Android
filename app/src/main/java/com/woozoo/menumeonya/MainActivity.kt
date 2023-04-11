376fe9f59bca2ce330532a42d9016b83500a686d
zzanzu <lees3301@gmail.com>
2023-04-12 00:57:09 +0900

    feat: 권한 요청 로직 실행 타이밍 변경, 현재 위치 이동 로직 구현(아직 오류 존재함)

--- app/src/main/java/com/woozoo/menumeonya/MainActivity.kt
+++ app/src/main/java/com/woozoo/menumeonya/MainActivity.kt
@@ -1,18 +1,77 @@
 package com.woozoo.menumeonya
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import net.daum.mf.map.api.MapView.CurrentLocationEventListener
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+    private lateinit var currentLocationButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mapView = MapView(this)
+        currentLocationButton = findViewById(R.id.my_location_button)
+        currentLocationButton.setOnClickListener {
+            mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading  //이 부분
+
+            val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+                val userNowLocation: Location? =
+                    lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+                if (userNowLocation != null) {
+                    val uLatitude = userNowLocation.latitude
+                    val uLongitude = userNowLocation.longitude
+                    val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+                    mapView.setMapCenterPoint(uNowPosition, true)
+                }
+
+            }
+        }
+
+        // Kakao 지도 SDK 초기화
+        mapView = MapView(this)
         val mapViewContainer = findViewById<View>(R.id.map_view_layout) as ViewGroup
         mapViewContainer.addView(mapView)
+
+        setMapViewListener()
+    }
+
+    private fun setMapViewListener() {
+        mapView.setCurrentLocationEventListener(object: CurrentLocationEventListener {
+            override fun onCurrentLocationUpdate(p0: MapView?, p1: MapPoint?, p2: Float) {
+                Log.d("zzanzu", "onCurrentLocationUpdate: $p1")
+                mapView.setMapCenterPoint(p1, true)
+            }
+
+            override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+                Log.d("zzanzu", "onCurrentLocationDeviceHeadingUpdate: ")
+            }
+
+            override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+                Log.d("zzanzu", "onCurrentLocationUpdateFailed: ")
+            }
+
+            override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+                Log.d("zzanzu", "onCurrentLocationUpdateCancelled: ")
+            }
+
+        })
     }
 }
