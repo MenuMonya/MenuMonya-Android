df451b1568acdd1befc87fc18e5e816c1a47b825
Chanjoo Lee <lees3301@gmail.com>
2023-04-12 20:43:45 +0900

    feat: 네이버 지도 SDK 세팅 (#1)
    * feat: 네이버 맵 세팅
    
    * chore: gitignore 파일 수정
    
    * feat: 네이버 맵 기본 로직 구현(위치 조회, 카메라 이동)
    
    * chore: 카카오맵 관련 코드 제거

---
+++ app/src/main/java/com/woozoo/menumeonya/NaverActivity.kt
@@ -0,0 +1,131 @@
+package com.woozoo.menumeonya
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.util.FusedLocationSource
+
+class NaverActivity : AppCompatActivity(), LocationListener {
+
+    private lateinit var mapView: MapView
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    private lateinit var locationManager: LocationManager
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_naver)
+
+        mapView = findViewById(R.id.naver_map)
+        mapView.onCreate(savedInstanceState)
+
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        mapView.getMapAsync {
+            this.naverMap = it
+            naverMap.locationSource = locationSource
+            naverMap.locationTrackingMode = LocationTrackingMode.NoFollow
+
+            val uiSetting = naverMap.uiSettings
+            uiSetting.isLocationButtonEnabled = true
+
+            if (checkPermission()) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 1000, 10f, this)
+            }
+        }
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+
+        if (checkPermission()) {
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000, 10f, this)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        if (location == null) {
+            return
+        }
+
+        val coord = LatLng(location)
+
+        val locationOverlay = naverMap.locationOverlay
+        locationOverlay.isVisible = true
+        locationOverlay.position = coord
+        locationOverlay.bearing = location.bearing
+
+        naverMap.moveCamera(CameraUpdate.scrollTo(coord))
+    }
+}
