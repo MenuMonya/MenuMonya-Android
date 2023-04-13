4a40264911e717f19765482fa336bf433d8c7cfa
zzanzu <lees3301@gmail.com>
2023-04-13 21:13:06 +0900

    feat: ViewModel로 Activity에 정의된 지도 관련 로직 이동

--- app/src/main/java/com/woozoo/menumeonya/MainActivity.kt
+++ app/src/main/java/com/woozoo/menumeonya/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.woozoo.menumeonya
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.CameraUpdate
-import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity(), LocationListener {
+class MainActivity : AppCompatActivity() {
 
-    val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
     private lateinit var mapView: MapView
-    private lateinit var locationSource: FusedLocationSource
-    private lateinit var naverMap: NaverMap
-
-    private lateinit var locationManager: LocationManager
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,24 +22,9 @@ class MainActivity : AppCompatActivity(), LocationListener {
         mapView = findViewById(R.id.naver_map)
         mapView.onCreate(savedInstanceState)
 
-        locationSource =
-            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-
-        mapView.getMapAsync {
-            this.naverMap = it
-            naverMap.locationSource = locationSource
-            naverMap.locationTrackingMode = LocationTrackingMode.NoFollow
-
-            val uiSetting = naverMap.uiSettings
-            uiSetting.isLocationButtonEnabled = true
-
-            if (checkPermission()) {
-                locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 1000, 10f, this)
-            }
+        viewModel.initializeMapView(mapView, this) {
+            viewModel.moveCamera(it)
         }
-
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
 //    private fun handleEvent(event: MainViewModel.Event) = when (event) {
@@ -67,35 +32,9 @@ class MainActivity : AppCompatActivity(), LocationListener {
          * is Event.ShowToast -> Toast.makeText(this, event.text, Toast.LENGTH_SHORT).show()
          */
 //    }
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
-                grantResults)) {
-            if (!locationSource.isActivated) { // 권한 거부됨
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-            }
-            return
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-
-
     override fun onStart() {
         super.onStart()
         mapView.onStart()
-
-        if (checkPermission()) {
-            locationManager.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER, 1000, 10f, this)
-        }
-    }
-
-    private fun checkPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(this,
-            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED
     }
 
     override fun onResume() {
@@ -126,20 +65,5 @@ class MainActivity : AppCompatActivity(), LocationListener {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
-    }
-
-    override fun onLocationChanged(location: Location) {
-        if (location == null) {
-            return
-        }
-
-        val coord = LatLng(location)
-
-        val locationOverlay = naverMap.locationOverlay
-        locationOverlay.isVisible = true
-        locationOverlay.position = coord
-        locationOverlay.bearing = location.bearing
-
-        naverMap.moveCamera(CameraUpdate.scrollTo(coord))
     }
 }
--- app/src/main/java/com/woozoo/menumeonya/MainViewModel.kt
+++ app/src/main/java/com/woozoo/menumeonya/MainViewModel.kt
@@ -1,19 +1,72 @@
 package com.woozoo.menumeonya
 
-import androidx.lifecycle.ViewModel
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Application
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.util.FusedLocationSource
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel(application: Application): AndroidViewModel(Application()) {
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1000
+
     private val _eventFlow = MutableSharedFlow<Event>()
     val eventFlow = _eventFlow.asSharedFlow()
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationManager: LocationManager
+
+    init {
+        locationManager = application.getSystemService(LOCATION_SERVICE) as LocationManager
+    }
 
     private fun event(event: Event) {
         viewModelScope.launch {
             _eventFlow.emit(event)
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun initializeMapView(mapView: MapView, activity: Activity, listener: LocationListener) {
+        mapView.getMapAsync {
+            naverMap = it.apply {
+                locationSource = FusedLocationSource(
+                    activity,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+                locationTrackingMode = LocationTrackingMode.NoFollow
+                uiSettings.isLocationButtonEnabled = true
+            }
+
+            val currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            if (currentLocation != null) {
+                moveCamera(currentLocation!!)
+            }
+        }
+    }
+
+    fun moveCamera(location: Location) {
+        val coord = LatLng(location)
+
+        naverMap.locationOverlay.apply {
+            isVisible = true
+            position = coord
+            bearing = location.bearing
+        }
+
+        naverMap.moveCamera(CameraUpdate.scrollTo(coord))
     }
 
     sealed class Event {
