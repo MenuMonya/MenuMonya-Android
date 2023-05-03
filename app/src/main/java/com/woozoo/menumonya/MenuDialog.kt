c42964ad7c9cb23796f0c0a64e897e0978a5580f
zzanzu <lees3301@gmail.com>
2023-05-03 22:14:20 +0900

    fix: 메뉴 다이얼로그의 width가 일정하지 않은 현상 해결(#26)
    - style 적용 + onCreate()에서 width 설(

--- app/src/main/java/com/woozoo/menumonya/MenuDialog.kt
+++ app/src/main/java/com/woozoo/menumonya/MenuDialog.kt
@@ -5,10 +5,11 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import com.woozoo.menumonya.databinding.DialogMenuBinding
 import com.woozoo.menumonya.model.Restaurant
 
-class MenuDialog(context: Context, val data: Restaurant) : Dialog(context) {
+class MenuDialog(context: Context, val data: Restaurant) : Dialog(context, R.style.custom_style_dialog) {
     private lateinit var binding: DialogMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +19,19 @@ class MenuDialog(context: Context, val data: Restaurant) : Dialog(context) {
         binding = DialogMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setDialogWidthMatchParent()
+
         binding.restaurantMenuMainTv.text = data.todayMenu.main.replace(",", ", ")
         binding.restaurantMenuSideTv.text = data.todayMenu.side.replace(",", ", ")
         binding.restaurantMenuDessertTv.text = data.todayMenu.dessert.replace(",", ", ")
     }
 
-
+    /**
+     * (#26)
+     * 커스텀 다이얼로그 특성상 xml에서 width, height에 MATCH_PARENT가 적용되지 않아 코드로 적용함.
+     */
+    fun setDialogWidthMatchParent() {
+        val layoutParams = window?.attributes
+        layoutParams?.width = MATCH_PARENT
+    }
 }
