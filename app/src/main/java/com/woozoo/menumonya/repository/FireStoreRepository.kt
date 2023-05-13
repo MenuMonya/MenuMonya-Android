775a96b19182fa99ea54582e8afec782bfd7a8c5
zzanzu <lees3301@gmail.com>
2023-05-13 16:45:36 +0900

    fix: 메뉴 조회 쿼리 로직 제거
    - restaurant 다큐먼트에 메뉴 정보 추가되어 별도로 조회할 필요 없음.

--- app/src/main/java/com/woozoo/menumonya/repository/FireStoreRepository.kt
+++ app/src/main/java/com/woozoo/menumonya/repository/FireStoreRepository.kt
@@ -1,11 +1,8 @@
 package com.woozoo.menumonya.repository
 
-import com.woozoo.menumonya.model.Menu
 import com.woozoo.menumonya.model.Restaurant
 
 interface FireStoreRepository {
 
     suspend fun getRestaurantInLocation(location: String): ArrayList<Restaurant>
-
-    suspend fun getMenu(restaurantId: String): Menu
 }
